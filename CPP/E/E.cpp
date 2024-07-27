830ce889001c8554f006e04bf1506475cd15080b
ABC1232121 <ujlow885@gmail.com>
2024-07-27 14:35:53 +0900

    EX4AC

---
+++ CPP/E/E.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 変数
+int main() {
+    int name;
+    name = 10;
+    cout << name << endl;
+    cout << name + 2 << endl;
+    cout << name * 3 << endl;
+}
