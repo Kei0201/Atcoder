a875eedddd0f4b8d9be4d6ef0fae51c4b44d24a5
ABC1232121 <ujlow885@gmail.com>
2024-07-27 18:49:39 +0900

    EX7AC

---
+++ CPP/H/H.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    // 1は真を表すのでhelloと出力
+    if (1) {
+        cout << "hello" << endl;
+    }
+
+    // 0は偽を表すのでこのifの中は実行されない
+    if (0) {
+        cout << "world" << endl;
+    }
+}
