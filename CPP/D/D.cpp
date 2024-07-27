71d629a4c1740bdf2e9a38bcdb7e009c7d85cdf7
ABC1232121 <ujlow885@gmail.com>
2024-07-27 14:25:42 +0900

    EX3完了

---
+++ CPP/D/D.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    // 四則演算
+    cout << 1 + 1 << endl;
+    cout << 3 - 4 << endl;
+    cout << 2 * 3 << endl;
+    cout << 7 / 3 << endl;
+    // 余剰演算
+    cout << 7 % 3 << endl;
+    cout << 4 % 5 << endl;
+}
+
+// 0で割るとエラーが発生する（0除算）
