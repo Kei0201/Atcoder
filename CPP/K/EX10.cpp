3a3cdff4f60ab23a7940b1b6f530d9586350ae85
ABC1232121 <ujlow885@gmail.com>
2024-07-27 19:48:12 +0900

    EX10AC

---
+++ CPP/K/EX10.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int A, B;
+    cin >> A >> B;
+
+    
+    // プログラムを追記
+    cout << "A:";
+    while (A > 0) {
+        cout << "]";
+        A--;
+    }
+    cout << endl;
+
+    cout << "B:";
+    while (B > 0) {
+        cout << "]";
+        B--;
+    }
+    cout << endl;
+}
