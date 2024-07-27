5a4b36f74b16c746e66cc44b30f2a7d09459d18c
ABC1232121 <ujlow885@gmail.com>
2024-07-27 19:37:34 +0900

    EX8AC

---
+++ CPP/I/I.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 変数のスコープ
+int main () {
+    int x = 5;
+    cout << x << endl;
+
+    if (x == 5) { 
+        cout << x << endl;
+
+        string x = "hello";
+        cout << x << endl;
+    }
+    cout << x << endl;
+}
