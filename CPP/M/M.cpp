0454145bb606c07143c614e8c1697572e46ebee3
ABC1232121 <ujlow885@gmail.com>
2024-07-28 18:44:16 +0900

    2024/07/28

---
+++ CPP/M/M.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1;
+    str2 = ", world";
+    // cout << str1 << str2 << endl;
+
+    // cout << str1.size() << endl;
+
+    // cout << str1.at(1) << endl;
+
+    char c = 'a';
+    cout << c << endl;
+}
