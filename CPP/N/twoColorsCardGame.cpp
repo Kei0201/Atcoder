a881c6ad771db0747c0f397ea095e531d4d549eb
ABC1232121 <ujlow885@gmail.com>
2024-08-03 19:27:57 +0900

    WA

---
+++ CPP/N/twoColorsCardGame.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n, m;
+    cin >> n;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++) cin >> s[i];
+    cin >> m;
+    vector<string> t(m);
+    for (int i = 0; i < m; i++) cin >> t[i];
+
+    // WA       
+}
