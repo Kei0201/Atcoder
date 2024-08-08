5a2549a7fe1e64c992e56b0358ff7fd0e0aaaab1
ABC1232121 <ujlow885@gmail.com>
2024-08-09 00:11:42 +0900

    cardGameForTwo AC

--- CPP/O/cardGameForTwo.cpp
+++ CPP/O/cardGameForTwo.cpp
@@ -7,5 +7,16 @@ int main () {
     vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a.at(i);
 
-    
-}
+    int alice = 0, bob = 0;
+    sort(a.begin(), a.end());
+    reverse(a.begin(), a.end());
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            alice += a.at(i);
+        } else {
+            bob += a.at(i);
+        }
+    }
+    cout << alice - bob << endl;
+}   
